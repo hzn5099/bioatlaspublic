4811d8e61413072a913d0f090285e7d88970a9df
khngu127 <khoanguyen3333@gmail.com>
2024-02-27 17:34:56 -0500

    updated virtualslide_tiles table

--- php/bioatlas/zfatlasDump/labels/inc/connect+.php
+++ php/bioatlas/zfatlasDump/labels/inc/connect+.php
@@ -8,6 +8,7 @@ require 'config.php';
 // ------------------------------------------------------------------------
 $zfatlas_db = mysqli_init();
 mysqli_options($zfatlas_db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+$db_ssl_ca = '';
 mysqli_ssl_set($zfatlas_db, null, null, $db_ssl_ca, null , null);
 mysqli_real_connect($zfatlas_db, $zf_mysql_host, $zf_mysql_admin, $zf_mysql_admin_pass, $zf_mysql_db, $zf_mysql_port, null, 64);
 
--- php/bioatlas/zfatlasDump/labels/tile.jpeg.php
+++ php/bioatlas/zfatlasDump/labels/tile.jpeg.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
 
 if (isset($_GET['s'], $_GET['z'], $_GET['i'])
     && ctype_digit($_GET['s'])
@@ -10,17 +12,22 @@ if (isset($_GET['s'], $_GET['z'], $_GET['i'])
 
     $query ='SELECT SQL_NO_CACHE image FROM virtualslide_tiles WHERE slide_id=%d AND level=%d AND tile_id=%d';
     $result = zf_mysql_query($query, $_GET['s'], $_GET['z'], $_GET['i']);
+    var_dump($result);
+    echo "s:" . $_GET['s'] . "z:" . $_GET['z'] . "i:" . $_GET['i'];
 
     if (!$result) {
 
+        echo("Reached 1");
         header('HTTP/1.1 503 Service Unavailable', true, 503);
 
     } else if (!mysqli_num_rows($result)) {
 
+        echo("Reached 2");
         header('HTTP/1.1 404 Not Found', true, 404);
 
     } else {
 
+        echo("Reached 3");
         header('Content-type: image/jpeg');
         $row = mysqli_fetch_row($result);
         echo $row[0];
@@ -29,6 +36,7 @@ if (isset($_GET['s'], $_GET['z'], $_GET['i'])
 
 } else {
 
+    echo("Reached 4");
     header('HTTP/1.1 400 Bad Request', true, 400);
 
 }
