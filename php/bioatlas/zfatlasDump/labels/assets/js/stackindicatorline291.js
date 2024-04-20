1a5b3f5f28e213e3f5375c338366d20b6066666e
Dylan Martin <dfm5288@psu.edu>
2024-04-20 18:26:03 -0400

    Added files for Sagittal & Transverse 3dpf

---
+++ php/bioatlas/zfatlasDump/labels/assets/js/stackindicatorline291.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var stackViewer = OpenSeadragon({
+        id: "stack_indicator_line",
+        prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
+        tileSources: "http://localhost:8000/assets/EC2/output/anatomical_reference_histology_zf/Coronal_3dpf_svg/dzi_output/291.dzi",
+        showNavigationControl: false
+    });
+});
---
+++ php/bioatlas/zfatlasDump/labels/assets/js/toolbar494.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('nav_left_10').addEventListener('click', function() {
+        window.location.href = 'http://localhost:8000/view.php?atlas=20&s=493';
+    });
+    document.getElementById('nav_left_1').addEventListener('click', function() {
+        window.location.href = 'http://localhost:8000/view.php?atlas=20&s=493';
+    });
+    document.getElementById('nav_right_1').addEventListener('click', function() {
+        window.location.href = 'http://localhost:8000/view.php?atlas=20&s=496';
+    });
+    document.getElementById('nav_right_10').addEventListener('click', function() {
+        window.location.href = 'http://localhost:8000/view.php?atlas=20&s=496';
+    });
+});
